eabbe73dc583dba20b635a15b458c68a07d94404
Zach <zboyle@umich.edu>
2020-04-05 03:22:39 -0400

    restructured code for easy config, testing, etc
    * added .yml config file for experimenting and
      easy switching between different cells
    * fleshed out testing structure to support both
      system simulation and unit tests
    * created e-longboard python package for easy
      installation on new computers
    * moved blynk token to environment variable
      for security reasons

---
+++ app/bms.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+#imports here
+
+# function defs here
+
+# main loop
+if __name__ == '__main__':
+	while True:
+		pass
---
+++ validation/tests/test_bms.py
@@ -0,0 +1,41 @@
+import psutil
+from subprocess import PIPE
+import time
+from app.bms import *
+
+class TestSimBms:
+
+    def sim_sleep(self, ms):
+        '''stop test execution and resume bms script for ms milliseconds'''
+        seconds = ms / 1000
+        self.bms_process.resume()
+        time.sleep(seconds)
+        self.bms_process.suspend()
+
+    @classmethod
+    def setup(self):
+        self.bms_process = psutil.Popen(['./app/bms.py'])
+        self.bms_process.suspend()
+
+    @classmethod
+    def teardown(self):
+        assert(self.bms_process.status() == psutil.STATUS_STOPPED)
+        self.bms_process.kill()
+
+    def test_one(self):
+        print('in sim test one!')
+        self.sim_sleep(4000)
+        print('in sim test one after sleep!')
+
+class TestUnitBms:
+
+    @classmethod
+    def setup(self):
+        pass
+
+    @classmethod
+    def teardown(self):
+        pass
+
+    def test_one(self):
+        print('in unit test one!')
